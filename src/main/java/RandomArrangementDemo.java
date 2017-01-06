79a2cc50c6730e2a15d532c400dfbb25a0aefa67
ScorpionOrange <scorpionorange@126.com>
2017-01-07 01:18:11 +0800

    Initial commit

---
+++ src/main/java/RandomArrangementDemo.java
@@ -0,0 +1,27 @@
+/**
+ * Created by ScorpionOrange on 2017/01/05.
+ */
+public class RandomArrangementDemo {
+    public static void main(String[] args){
+        int a = 5, b = 2;
+        System.out.print("a = 5, b = 2, 交换后：");
+        /*
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        */
+        swap(a, b);
+        System.out.println("a = " + a + ", b = " + b);
+    }
+
+    public static void randomArrangement(long[] arrayInput){
+    }
+
+    public static void swap(int a, int b){
+        /*
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        */
+    }
+}
