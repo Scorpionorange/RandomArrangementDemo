0cb447e432f5b3083ceb5bd32272db34fb063ed6
ScorpionOrange <scorpionorange@126.com>
2017-01-07 01:58:27 +0800

    from Object[ ] to int[ ], had no way to using Object[ ] at this time.

--- src/main/java/RandomArrangementDemo.java
+++ src/main/java/RandomArrangementDemo.java
@@ -1,27 +1,31 @@
+import java.util.Arrays;
+
 /**
  * Created by ScorpionOrange on 2017/01/05.
  */
 public class RandomArrangementDemo {
     public static void main(String[] args){
-        int a = 5, b = 2;
-        System.out.print("a = 5, b = 2, 交换后：");
-        /*
-        a = a + b;
-        b = a - b;
-        a = a - b;
-        */
-        swap(a, b);
-        System.out.println("a = " + a + ", b = " + b);
+        int[] a = new int[20];
+        for(int i = 0; i < a.length; i++){
+            a[i] = (int)(Math.random()*1000);
+        }
+        Arrays.sort(a);
+        for(int temp : a){
+            System.out.print(temp + "  ");
+        }
+        System.out.println("\nAfter :");
+        randomArrangement(a);
+        for(int temp : a){
+            System.out.print(temp + "  ");
+        }
     }
 
-    public static void randomArrangement(long[] arrayInput){
-    }
-
-    public static void swap(int a, int b){
-        /*
-        a = a + b;
-        b = a - b;
-        a = a - b;
-        */
+    public static void randomArrangement(int[] arrayInput){
+        for(int i = 0; i < arrayInput.length; i++){
+            int temp = arrayInput[i];
+            int tempNumber = (int) (Math.random()*arrayInput.length);
+            arrayInput[i] = arrayInput[tempNumber];
+            arrayInput[tempNumber] = temp;
+        }
     }
 }
